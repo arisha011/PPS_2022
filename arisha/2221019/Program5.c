ecfa947c56ead9a318e7cacc7e75f67f4dfab9bc
arisha011 <116053202+arisha011@users.noreply.github.com>
2022-11-01 10:34:50 +0530

    Create Program5.c

---
+++ arisha/2221019/Program5.c
@@ -0,0 +1,14 @@
+## Program5: Write a program using conditional operators 
+```
+# include <stdio.h>
+int main()
+{ 
+ int T=95,PT=95,C;
+ C=(T>80&&PT>80)?1000:(PT>80)?500:0;
+ printf("%d", C);
+ return 0;
+}
+ 
+output : 1000
+  
+
