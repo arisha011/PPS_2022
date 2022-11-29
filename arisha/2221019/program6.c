98a8b80a95338dee590996a18d5e4e32e3bbb05f
arisha011 <116053202+arisha011@users.noreply.github.com>
2022-11-29 09:08:47 +0530

    Create program6.c

---
+++ arisha/2221019/program6.c
@@ -0,0 +1,14 @@
+## Program 6 – write a program using for loop
+
+Input 
+#include <stdio.h>
+int main()
+{
+for(int i=0 ; i<10 ; i=i+1)
+printf("i am sorry/n");
+return 0;
+} 
+
+output 
+i am sorry/ni am sorry/ni am sorry/ni am sorry/ni am sorry/ni am sorry/ni am sorry/ni am sorry/ni am sorry/ni am sorry/
+      
