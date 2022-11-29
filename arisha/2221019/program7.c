e00b7da74fd440fc4c3542ffaf2e88a5f4737225
arisha011 <116053202+arisha011@users.noreply.github.com>
2022-11-29 09:17:51 +0530

    Update program7.c

--- arisha/2221019/program7.c
+++ arisha/2221019/program7.c
@@ -1,4 +1,13 @@
-program 7 : write a program using while loop.
-input 
-output 
-i am sorry/ni am sorry/ni am sorry/ni am sorry/ni am sorry/ni am sorry/ni am sorry/ni am sorry/ni am sorry/ni am sorry/
+## Program 7 : write a program using while loop.
+```  
+#include <stdio.h>
+int main()
+{
+int i=4;
+while (i<10)
+{printf("%d",i);
+i++;
+}
+return 0;
+}
+```
