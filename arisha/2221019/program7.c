ad0bf43fee18e1074dc277756f667e5bfc10d0b9
arisha011 <116053202+arisha011@users.noreply.github.com>
2022-11-29 09:11:18 +0530

    Create program7.c

---
+++ arisha/2221019/program7.c
@@ -0,0 +1,4 @@
+program 7 : write a program using while loop.
+input 
+output 
+i am sorry/ni am sorry/ni am sorry/ni am sorry/ni am sorry/ni am sorry/ni am sorry/ni am sorry/ni am sorry/ni am sorry/
